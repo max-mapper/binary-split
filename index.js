8eac05e3c329d904fd0ca72570c1072a0e933f56
Vladimir Agafonkin <agafonkin@gmail.com>
2016-03-16 16:16:49 +0200

    Revert "simplify match function"
    This reverts commit cc5b1dac01e38072cf413959c62810fa4e42a03e which was causing problems in some downstream modules, not caught by the test suite — I will try to follow with a test case and figure out what's the problem.

--- index.js
+++ index.js
@@ -42,13 +42,24 @@ function BinarySplit (matcher) {
 
   function firstMatch (buf, offset) {
     if (offset >= buf.length) return -1
-
-    for (var i = offset, m = matcher.length; i < buf.length - m; i++) {
-      for (var j = 0; j < m; j++) {
-        if (buf[i + j] !== matcher[j]) break
+    for (var i = offset; i < buf.length; i++) {
+      if (buf[i] === matcher[0]) {
+        if (matcher.length > 1) {
+          var fullMatch = true
+          for (var j = i, k = 0; j < i + matcher.length; j++, k++) {
+            if (buf[j] !== matcher[k]) {
+              fullMatch = false
+              break
+            }
+          }
+          if (fullMatch) return j - matcher.length
+        } else {
+          break
+        }
       }
-      if (j === m) return i
     }
-    return buf.length
+
+    var idx = i + matcher.length - 1
+    return idx
   }
 }
