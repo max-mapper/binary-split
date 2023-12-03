539341be6afbf65175969bc9909dea14bb213788
Vladimir Agafonkin <agafonkin@gmail.com>
2023-12-03 15:27:05 +0200

    simplify, switch from through2 to core stream

--- index.js
+++ index.js
@@ -1,68 +1,69 @@
-const through = require('through2')
-const os = require('os')
+const { Transform } = require('stream')
+const { EOL } = require('os')
 
 module.exports = BinarySplit
 
-function BinarySplit (splitOn) {
-  if (!(this instanceof BinarySplit)) return new BinarySplit(splitOn)
-  splitOn = splitOn || os.EOL
-  const matcher = Buffer.from(splitOn)
-  let buffered
-  return through({ readableObjectMode: true }, write, end)
-
-  function write (buf, enc, done) {
-    let offset = 0
-    let lastMatch = 0
-    if (buffered) {
-      buf = Buffer.concat([buffered, buf])
-      offset = buffered.length
-      buffered = undefined
-    }
-
-    while (true) {
-      const idx = firstMatch(buf, offset - matcher.length + 1)
-      if (idx !== -1 && idx < buf.length) {
-        if (lastMatch !== idx) {
-          this.push(buf.slice(lastMatch, idx))
+function firstMatch (buf, offset, matcher) {
+  if (offset >= buf.length) return -1
+  let i
+  for (i = offset; i < buf.length; i++) {
+    if (buf[i] === matcher[0]) {
+      if (matcher.length > 1) {
+        let fullMatch = true
+        let j = i
+        for (let k = 0; j < i + matcher.length; j++, k++) {
+          if (buf[j] !== matcher[k]) {
+            fullMatch = false
+            break
+          }
         }
-        offset = idx + matcher.length
-        lastMatch = offset
+        if (fullMatch) return j - matcher.length
       } else {
-        buffered = buf.slice(lastMatch)
         break
       }
     }
-
-    done()
   }
 
-  function end (done) {
-    if (buffered && buffered.length > 0) this.push(buffered)
-    done()
-  }
+  const idx = i + matcher.length - 1
+  return idx
+}
 
-  function firstMatch (buf, offset) {
-    if (offset >= buf.length) return -1
-    let i
-    for (i = offset; i < buf.length; i++) {
-      if (buf[i] === matcher[0]) {
-        if (matcher.length > 1) {
-          let fullMatch = true
-          let j = i
-          for (let k = 0; j < i + matcher.length; j++, k++) {
-            if (buf[j] !== matcher[k]) {
-              fullMatch = false
-              break
-            }
+function BinarySplit (splitOn = EOL) {
+  const matcher = Buffer.from(splitOn)
+  let buffered
+
+  return new Transform({
+    readableObjectMode: true,
+
+    transform (buf, enc, done) {
+      let offset = 0
+      let lastMatch = 0
+      if (buffered) {
+        buf = Buffer.concat([buffered, buf])
+        offset = buffered.length
+        buffered = undefined
+      }
+
+      while (true) {
+        const idx = firstMatch(buf, offset - matcher.length + 1, matcher)
+        if (idx !== -1 && idx < buf.length) {
+          if (lastMatch !== idx) {
+            this.push(buf.slice(lastMatch, idx))
           }
-          if (fullMatch) return j - matcher.length
+          offset = idx + matcher.length
+          lastMatch = offset
         } else {
+          buffered = buf.slice(lastMatch)
           break
         }
       }
-    }
 
-    const idx = i + matcher.length - 1
-    return idx
-  }
+      done()
+    },
+
+    flush (done) {
+      if (buffered && buffered.length > 0) this.push(buffered)
+      done()
+    }
+  })
 }
